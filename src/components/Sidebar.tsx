a80718f1a09f0b82e6e8cd3748ae88eb981b8151
david <davidwdiniz@gmail.com>
2023-01-11 14:01:20 -0300

    Playlist navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,54 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  LinkBox,
+  LinkOverlay,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
 import Image from "next/image";
+import {
+  MdFavorite,
+  MdHome,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdSearch,
+} from "react-icons/md";
+import Link from "next/link";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
+
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
+
+const playlists = new Array(30).fill(null).map((_, i) => `Playlist ${i + 1}`);
 
 const Sidebar = () => {
   return (
@@ -10,7 +59,7 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <Image
             src="/logo.svg"
@@ -18,6 +67,59 @@ const Sidebar = () => {
             width={120}
             height={60}
           />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing="2">
+            {navMenu.map((menu) => (
+              <ListItem key={menu.name} paddingX="20px" fontSize="16px">
+                <LinkBox>
+                  <LinkOverlay as={Link} href={menu.route} passHref>
+                    <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                    {menu.name}
+                  </LinkOverlay>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box>
+          <List spacing="2">
+            {musicMenu.map((menu) => (
+              <ListItem key={menu.name} paddingX="20px" fontSize="16px">
+                <LinkBox>
+                  <LinkOverlay as={Link} href={menu.route} passHref>
+                    <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                    {menu.name}
+                  </LinkOverlay>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box paddingX="20px" marginY="20px">
+          <Divider color="gray.800" />
+        </Box>
+        <Box
+          height="66%"
+          overflowY="auto"
+          paddingY="20px"
+          sx={{
+            "&::-webkit-scrollbar": {
+              display: "none",
+            },
+          }}
+        >
+          <List spacing="2">
+            {playlists.map((playlist) => (
+              <ListItem key={playlist} paddingX="20px" fontSize="16px">
+                <LinkBox>
+                  <LinkOverlay as={Link} href="/" passHref>
+                    {playlist}
+                  </LinkOverlay>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </Box>
     </Box>
